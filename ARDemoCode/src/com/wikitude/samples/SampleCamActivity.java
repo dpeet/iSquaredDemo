7c2755a13140ee8dac897ece94475ba4696d95fe
Dpeet <dpeet@gatech.edu>
2013-11-12 00:34:48 -0500

    jumps straight into test world now

--- ARDemoCode/src/com/wikitude/samples/SampleCamActivity.java
+++ ARDemoCode/src/com/wikitude/samples/SampleCamActivity.java
@@ -26,25 +26,12 @@ public class SampleCamActivity extends AbstractArchitectCamActivity {
 	/**
 	 * last time the calibration toast was shown, this avoids too many toast shown when compass needs calibration
 	 */
-	/*
-	public void onCreate( Bundle savedInstanceState ) {
-		super.onCreate( savedInstanceState );
-		this.setContentView( this.getContentViewId() );
-		
-		// check if AR.VideoDrawables are supported on the current device. if not -> show hint-Toast message
-				if (activitiesToLaunch.get(0).categoryId == 6 && ! MainActivity.isVideoDrawablesSupported()) {
-					Toast.makeText(this, R.string.videosrawables_fallback, Toast.LENGTH_LONG).show();
-				}
-		
-		
-	}
-	*/
 	private long lastCalibrationToastShownTimeMillis = System.currentTimeMillis();
 
 	@Override
 	public String getARchitectWorldPath() {
-		return getIntent().getExtras().getString(
-				EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL);
+		return "3_Point$Of$Interest_3_Multiple$Pois";
+		//return getIntent().getExtras().getString(EXTRAS_KEY_ACTIVITY_ARCHITECT_WORLD_URL);
 	}
 
 	@Override
